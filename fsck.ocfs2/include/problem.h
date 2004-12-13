40045e4282ca6b217947363d26ed913e6ccfa43f
Zach Brown <zach.brown@oracle.com>
2004-12-13 23:06:30 +0000

    o add a cute target to test if any prompt() callers in fsck   specify the same error code
    Original-revision: 481

--- fsck.ocfs2/include/problem.h
+++ fsck.ocfs2/include/problem.h
@@ -32,9 +32,24 @@
 
 /* returns non-zero for yes and zero for no.  The caller is expected to
  * provide a thorough description of the state and the action that will
- * be taken depending on the answer.  Without \n termination. */
-int prompt(o2fsck_state *ost, unsigned flags, uint16_t code, 
-	   const char *fmt, ...)
+ * be taken depending on the answer.  Without \n termination.
+ *
+ * The code argument is a digit that identifies the error.  it is used by
+ * input to regression testing utils and referenced in fsck.ocfs2.checks(8).
+ * each code is only supposed to have one call site.  we create this
+ * funny symbol so that the 'check-prompt-callers' makefile target 
+ * can go groveling through nm to find out if a code has more than
+ * one call site.
+ */
+#define prompt(ost, flags, code, fmt...) ({			\
+	static int fsck_prompt_callers_with_code_##code = code;	\
+	int _ret = fsck_prompt_callers_with_code_##code;	\
+	_ret = prompt_input(ost, flags, code, fmt);		\
+	_ret;							\
+})
+
+int prompt_input(o2fsck_state *ost, unsigned flags, uint16_t code, 
+		 const char *fmt, ...)
 	 __attribute__ ((format (printf, 4, 5)));
 
 #endif /* __O2FSCK_PROBLEM_H__ */
