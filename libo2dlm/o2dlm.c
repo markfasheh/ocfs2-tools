f935a1ff7907d0ef9aaf5509d4a1c35a88641527
Mark Fasheh <mark.fasheh@oracle.com>
2005-03-21 21:18:15 +0000

    * fix up copyright info in o2dlm
    Signed-off-by: Mark Fasheh <mark.fasheh@oracle.com>
    Signed-off-by: Joel Becker <joel.becker@oracle.com>
    
    Original-revision: 723

--- libo2dlm/o2dlm.c
+++ libo2dlm/o2dlm.c
@@ -20,6 +20,8 @@
  * License along with this program; if not, write to the
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 021110-1307, USA.
+ *
+ * Authors: Mark Fasheh <mark.fasheh@oracle.com>
  */
 
 #include <stdio.h>
--- libo2dlm/o2dlm_test.c
+++ libo2dlm/o2dlm_test.c
@@ -1,5 +1,25 @@
-/*
+/* -*- mode: c; c-basic-offset: 8; -*-
+ * vim: noexpandtab sw=8 ts=8 sts=0:
+ *
  * Test driver for libo2dlm.
+ *
+ * Copyright (C) 2004 Oracle.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License, version 2,  as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 021110-1307, USA.
+ *
+ * Authors: Mark Fasheh <mark.fasheh@oracle.com>
  */
 
 #include <stdio.h>
