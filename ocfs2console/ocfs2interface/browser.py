0fa2633aace0c3e2afa9edc5ccf0145d3245b694
Manish Singh <manish.singh@oracle.com>
2005-04-08 08:43:11 +0000

    * tag for sles9 sp2 beta 1 release
    Original-revision: 791

--- ocfs2console/ocfs2interface/browser.py
+++ ocfs2console/ocfs2interface/browser.py
@@ -22,10 +22,6 @@ import ocfs2
 
 from guiutil import set_props
 
-fields = ('Perms', '# Links', 'Owner', 'Group', 'Size', 'Alloc Size',
-          'Timestamp', 'Name')
-sample = ('-rw-r--r--', '1', 'manish', 'manish', '133194', '262144', 'Sep 29 12:46', 'closobo.c')
-
 class Browser:
      def __init__(self, device=None):
          self.widget = gtk.VBox(spacing=4)
@@ -39,28 +35,6 @@ class Browser:
 
          tv.insert_column_with_attributes(-1, 'File', gtk.CellRendererText(),
                                           text=0)
-
-         table = gtk.Table(rows=2, columns=7)
-         set_props(table, row_spacing=4,
-                          column_spacing=4,
-                          border_width=4)
-         self.widget.pack_end(table, expand=False, fill=False)
-
-         font = pango.FontDescription('Monospace')
-
-         for i in range(0, len(fields)):
-             label = gtk.Label(fields[i])
-             set_props(label, xalign=0.0)
-             table.attach(label, i, i + 1, 0, 1)
-
-             label = gtk.Label(sample[i])
-             if i == 1 or i == 4 or i == 5:
-                 set_props(label, xalign=1.0)
-             else:
-                 set_props(label, xalign=0.0)
-             table.attach(label, i, i + 1, 1, 2)
-
-             label.modify_font(font)
 
 def main():
     import sys
