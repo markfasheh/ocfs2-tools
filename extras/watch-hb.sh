87768e222f9ead75bed2ed5c90ea50dff45dcb94
Sunil Mushran <sunil.mushran@oracle.com>
2005-04-12 01:08:31 +0000

    watch-hb accepts -d <dev> also
    Original-revision: 794

--- extras/watch-hb.sh
+++ extras/watch-hb.sh
@@ -7,29 +7,34 @@ die() {
 
 usage() {
 	echo "-r region		the hb region to watch"
+	echo "-d device		the hb device to watch"
 }
 
-while getopts ":r:" opt; do
+while getopts ":r:d:" opt; do
         case $opt in
                 r)
                         region="$OPTARG"
+			dev=$(cat $region/dev) || die "couldn't read $region/dev"
+			[ -z "$dev" ] && \
+				die "$region/dev isn't set, is the region active?"
+
+			eval "$device=/dev/$dev"
+
+			eval "slot_bytes=$(cat $region/$attr)" || die "couldn't read $region/$attr"
                         ;;
+		d)
+			dev="$OPTARG"
+			eval "device=$dev"
+			eval "slot_bytes=512"
+			;;
                 \?) usage
         esac
 done
 
-[ -z "$region" ] && \
-	die "a region must be specified with -r"
-
-dev=$(cat $region/dev) || die "couldn't read $region/dev"
-[ -z "$dev" ] && \
-	die "$region/dev isn't set, is the region active?"
-
-for attr in slot_bytes; do
-	eval "$attr=$(cat $region/$attr)" || die "couldn't read $region/$attr"
-done
+[ -z "$device" ] && \
+	die "a region must be specified with -r or a device with -d"
 
 watch -n 3 -d \
-	"echo \"cat //heartbeat\" | debugfs.ocfs2 /dev/$dev 2>/dev/null \
+	"echo \"cat //heartbeat\" | debugfs.ocfs2 $device 2>/dev/null \
 	| od -A d -x | \
 	awk '( NF > 2 ) { \$(1) = \$1 / $slot_bytes; if (\$1 == int(\$1)) print \$0 }'"
