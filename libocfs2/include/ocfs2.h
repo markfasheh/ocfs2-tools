c1194ce5d0fe09633657a9e459c3b927ef828edd
Joel Becker <joel.becker@oracle.com>
2007-11-15 13:39:02 -0800

    ocfs2_fs.h: Remove the feature strings.
    ocfs2_fs.h defines the on-disk format.  Printable strings for each
    feature flag belong in ocfs2.h
    
    Signed-off-by: Joel Becker <joel.becker@oracle.com>
    Mark Fasheh <mark.fasheh@oracle.com>

--- libocfs2/include/ocfs2.h
+++ libocfs2/include/ocfs2.h
@@ -85,6 +85,20 @@
 
 #define OCFS2_LIB_ABORTED_TUNEFS_SUPP		OCFS2_TUNEFS_INPROG_REMOVE_SLOT
 
+/*
+ * Printable strings for feature flags
+ * These MUST be kept in sync with the flags in ocfs2_fs.h, and MUST
+ * be named <flag>_STR.
+ */
+#define OCFS2_FEATURE_INCOMPAT_HEARTBEAT_DEV_STR	"Heartbeat"
+#define OCFS2_FEATURE_INCOMPAT_RESIZE_INPROG_STR	"AbortedResize"
+#define OCFS2_FEATURE_INCOMPAT_LOCAL_MOUNT_STR		"Local"
+#define OCFS2_FEATURE_INCOMPAT_SPARSE_ALLOC_STR		"SparseAlloc"
+#define OCFS2_FEATURE_INCOMPAT_TUNEFS_INPROG_STR	"TunefsAbort"
+#define OCFS2_FEATURE_COMPAT_BACKUP_SB_STR		"BackupSuper"
+#define OCFS2_FEATURE_RO_COMPAT_UNWRITTEN_STR		"UnwrittenExtents"
+#define OCFS2_TUNEFS_INPROG_REMOVE_SLOT_STR		"RemoveSlot"
+
 /* define OCFS2_SB for ocfs2-tools */
 #define OCFS2_SB(sb)	(sb)
 
