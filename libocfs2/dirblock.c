9bad03a06f6ea8aadeee46e3eacf349cc7fd4ae2
Joel Becker <joel.becker@oracle.com>
2008-12-17 17:53:27 -0800

    libocfs2: Initialize dirblock trailers when creating new dirblocks.
    libocfs2 creates new dirblocks in three places:
    
    - non-inline mkdir
    - expanding inline to a dirblock
    - adding a new dirblock
    
    All places now initialize a trailer if necessary.
    
    Signed-off-by: Joel Becker <joel.becker@oracle.com>

--- libocfs2/dirblock.c
+++ libocfs2/dirblock.c
@@ -74,6 +74,16 @@ int ocfs2_skip_dir_trailer(ocfs2_filesys *fs, struct ocfs2_dinode *di,
 	return 1;
 }
 
+void ocfs2_init_dir_trailer(ocfs2_filesys *fs, void *buf)
+{
+	struct ocfs2_dir_block_trailer *trailer =
+		ocfs2_dir_trailer_from_block(fs, buf);
+
+	memcpy(trailer->db_signature, OCFS2_DIR_TRAILER_SIGNATURE,
+	       strlen(OCFS2_DIR_TRAILER_SIGNATURE));
+	trailer->db_compat_rec_len = sizeof(struct ocfs2_dir_block_trailer);
+}
+
 static void ocfs2_swap_dir_entry(struct ocfs2_dir_entry *dirent)
 {
 	if (cpu_is_little_endian)
