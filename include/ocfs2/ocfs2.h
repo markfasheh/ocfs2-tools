markfasheh/ocfs2-tools

commit 52bae5e7a358e927a1e841ead2c6a95cf68c5db1

mount.ocfs2: Add support for o2cb global heartbeat
mount.ocfs2 is now o2cb global heartbeat-aware. It appends heartbeat=global
when mounting an ocfs2 volume in global heartbeat mode. It relies on the
o2cb framework to validate the heartbeat mode. It also only changes the io
priority for o2cb local heartbeat threads.

Signed-off-by: Sunil Mushran <sunil.mushran@oracle.com>
Acked-by: Joel Becker <jlbec@evilplan.org>

mount.ocfs2/mount.ocfs2.c
@@ -99,6 +99,56 @@ static void read_options(int argc, char **argv, struct mount_options *mo)
 }
 
 /*
+ * For local mounts, add heartbeat=none.
+ * For userspace clusterstack, add cluster_stack=xxxx.
+ * For o2cb with local heartbeat, add heartbeat=local.
+ * For o2cb with global heartbeat, add heartbeat=global.
+ */
+static errcode_t add_mount_options(ocfs2_filesys *fs,
+				   struct o2cb_cluster_desc *cluster,
+				   char **optstr)
+{
+	char *add, *extra = NULL;
+	char stackstr[strlen(OCFS2_CLUSTER_STACK_ARG) + OCFS2_STACK_LABEL_LEN + 1];
+	struct ocfs2_super_block *sb = OCFS2_RAW_SB(fs->fs_super);
+
+	if (ocfs2_mount_local(fs)) {
+		add = OCFS2_HB_NONE;
+		goto addit;
+	}
+
+	if (cluster->c_stack &&
+	    strcmp(cluster->c_stack, OCFS2_CLASSIC_CLUSTER_STACK)) {
+		snprintf(stackstr, sizeof(stackstr), "%s%s",
+			 OCFS2_CLUSTER_STACK_ARG, cluster->c_stack);
+		add = stackstr;
+		goto addit;
+	}
+
+	if (ocfs2_cluster_o2cb_global_heartbeat(sb)) {
+		add = OCFS2_HB_GLOBAL;
+		goto addit;
+	}
+
+	add = OCFS2_HB_LOCAL;
+
+addit:
+	if (optstr && *optstr) {
+		extra = xstrndup(*optstr, strlen(*optstr) + strlen(add) + 1);
+		if (extra)
+			extra = xstrconcat3(extra, ",", add);
+	} else
+		extra = xstrndup(add, strlen(add));
+
+	if (!extra)
+		return OCFS2_ET_NO_MEMORY;
+
+	*optstr = extra;
+
+	return 0;
+}
+
+/*
  * Code based on similar function in util-linux-2.12p/mount/mount.c
  *
  */
@@ -252,21 +302,36 @@ bail:
 	return ret;
 }
 
+static void change_local_hb_io_priority(ocfs2_filesys *fs, char *dev)
+{
+	struct ocfs2_super_block *sb = OCFS2_RAW_SB(fs->fs_super);
+	char hb_ctl_path[PATH_MAX];
+	errcode_t ret;
+
+	if (ocfs2_mount_local(fs))
+		return;
+
+	if (ocfs2_userspace_stack(sb))
+		return;
+
+	if (ocfs2_cluster_o2cb_global_heartbeat(sb))
+		return;
+
+	ret = o2cb_get_hb_ctl_path(hb_ctl_path, sizeof(hb_ctl_path));
+	if (!ret)
+		run_hb_ctl(hb_ctl_path, dev, "-P");
+}
 
 int main(int argc, char **argv)
 {
 	errcode_t ret = 0;
 	struct mount_options mo;
-	char hb_ctl_path[PATH_MAX];
-	char *extra = NULL;
 	int dev_ro = 0;
-	char *hbstr = NULL;
-	char stackstr[strlen(OCFS2_CLUSTER_STACK_ARG) + OCFS2_STACK_LABEL_LEN + 1];
 	ocfs2_filesys *fs = NULL;
 	struct o2cb_cluster_desc cluster;
 	struct o2cb_region_desc desc;
 	int clustered = 1;
-	int hb_started = 0;
+	int group_join = 0;
 	struct stat statbuf;
 
 	initialize_ocfs_error_table();
@@ -285,8 +350,6 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Could not set SIGINT\n");
 		exit(1);
 	}
-
-	*stackstr = '\0';
 
 	memset(&mo, 0, sizeof(mo));
 	read_options (argc, argv, &mo);
@@ -319,9 +382,6 @@ int main(int argc, char **argv)
 				"while trying to determine cluster information");
 			goto bail;
 		}
-		if (cluster.c_stack)
-			snprintf(stackstr, sizeof(stackstr), "%s%s",
-				 OCFS2_CLUSTER_STACK_ARG, cluster.c_stack);
 
 		ret = ocfs2_fill_heartbeat_desc(fs, &desc);
 		if (ret) {
@@ -331,13 +391,6 @@ int main(int argc, char **argv)
 		}
 		desc.r_persist = 1;
 		desc.r_service = OCFS2_FS_NAME;
-
-		ret = o2cb_get_hb_ctl_path(hb_ctl_path, sizeof(hb_ctl_path));
-		if (ret) {
-			com_err(progname, ret,
-				"probably because o2cb service not started");
-			goto bail;
-		}
 	}
 
 	if (mo.flags & MS_RDONLY) {
@@ -346,6 +399,12 @@ int main(int argc, char **argv)
 			com_err(progname, ret, "device not accessible");
 			goto bail;
 		}
+	}
+
+	ret = add_mount_options(fs, &cluster, &mo.xtra_opts);
+	if (ret) {
+		com_err(progname, ret, "while adding mount options");
+		goto bail;
 	}
 
 	block_signals (SIG_BLOCK);
@@ -358,27 +417,14 @@ int main(int argc, char **argv)
 				"while trying to join the group");
 			goto bail;
 		}
-		hb_started = 1;
+		group_join = 1;
 	}
 
-	if (dev_ro || !clustered)
-		hbstr = OCFS2_HB_NONE;
-	else if (strlen(stackstr))
-		hbstr = stackstr;
-	else
-		hbstr = OCFS2_HB_LOCAL;
-
-	if (mo.xtra_opts && *mo.xtra_opts) {
-		extra = xstrndup(mo.xtra_opts,
-				 strlen(mo.xtra_opts) + strlen(hbstr) + 1);
-		extra = xstrconcat3(extra, ",", hbstr);
-	} else
-		extra = xstrndup(hbstr, strlen(hbstr));
-
-	ret = mount(mo.dev, mo.dir, OCFS2_FS_NAME, mo.flags & ~MS_NOSYS, extra);
+	ret = mount(mo.dev, mo.dir, OCFS2_FS_NAME, mo.flags & ~MS_NOSYS,
+		    mo.xtra_opts);
 	if (ret) {
 		ret = errno;
-		if (hb_started) {
+		if (group_join) {
 			/* We ignore the return code because the mount
 			 * failure is the important error.
 			 * complete_group_join() will handle cleaning up */
@@ -402,7 +448,7 @@ int main(int argc, char **argv)
 				"error.", mo.dev, mo.dir);
 		goto bail;
 	}
-	if (hb_started) {
+	if (group_join) {
 		ret = o2cb_complete_group_join(&cluster, &desc, 0);
 		if (ret) {
 			com_err(progname, ret,
@@ -416,11 +462,12 @@ int main(int argc, char **argv)
 		}
 	}
 
-	run_hb_ctl (hb_ctl_path, mo.dev, "-P");
+	change_local_hb_io_priority(fs, mo.dev);
+
 	update_mtab_entry(mo.dev, mo.dir, OCFS2_FS_NAME,
 			  fix_opts_string(((mo.flags & ~MS_NOMTAB) |
 					   (clustered ? MS_NETDEV : 0)),
-					  extra, NULL),
+					  mo.xtra_opts, NULL),
 			  mo.flags, 0, 0);
 
 	block_signals (SIG_UNBLOCK);
@@ -428,8 +475,6 @@ int main(int argc, char **argv)
 bail:
 	if (fs)
 		ocfs2_close(fs);
-	if (extra)
-		free(extra);
 	if (mo.dev)
 		free(mo.dev);
 	if (mo.dir)

include/ocfs2-kernel/ocfs2_fs.h
@@ -370,6 +370,7 @@ static struct ocfs2_system_inode_info ocfs2_system_inodes[NUM_SYSTEM_INODES] = {
 /* Parameter passed from mount.ocfs2 to module */
 #define OCFS2_HB_NONE			"heartbeat=none"
 #define OCFS2_HB_LOCAL			"heartbeat=local"
+#define OCFS2_HB_GLOBAL			"heartbeat=global"
 
 /*
  * OCFS2 directory file types.  Only the low 3 bits are used.  The

commit 1bceaa23b3feac61ed419030c23fbc1501f26761

mount.ocfs2: Fix bugs related to hard-ro mounts
Patch teaches the libocfs2 call, ocfs2_open(), to detect hard-ro devices.

It also fixes a bug in mount.ocfs2, introduced by commit
52bae5e7a358e927a1e841ead2c6a95cf68c5db1, due to which it stopped appending
the mount option heartbeat=none for hard-ro mounts.

Signed-off-by: Sunil Mushran <sunil.mushran@oracle.com>
Reviewed-by: Joel Becker <jlbec@evilplan.org>

include/ocfs2/ocfs2.h
@@ -92,6 +92,7 @@
 #define OCFS2_FLAG_NO_ECC_CHECKS      0x0200	/* Do not validate metaecc
 						 * information on block
 						 * reads. */
+#define OCFS2_FLAG_HARD_RO            0x0400
 
 
 /* Return flags for the directory iterator functions */
@@ -291,6 +292,7 @@ errcode_t ocfs2_malloc_blocks(io_channel *channel, int num_blocks,
 			      void *ptr);
 errcode_t ocfs2_malloc_block(io_channel *channel, void *ptr);
 
+int io_is_device_readonly(io_channel *channel);
 errcode_t io_open(const char *name, int flags, io_channel **channel);
 errcode_t io_close(io_channel *channel);
 int io_get_error(io_channel *channel);
@@ -342,6 +344,7 @@ errcode_t ocfs2_read_blocks(ocfs2_filesys *fs, int64_t blkno, int count,
 			    char *data);
 errcode_t ocfs2_read_blocks_nocache(ocfs2_filesys *fs, int64_t blkno, int count,
 				    char *data);
+int ocfs2_is_hard_readonly(ocfs2_filesys *fs);
 int ocfs2_mount_local(ocfs2_filesys *fs);
 errcode_t ocfs2_open(const char *name, int flags,
 		     unsigned int superblock, unsigned int blksize,

libocfs2/openfs.c
@@ -264,6 +264,11 @@ int ocfs2_mount_local(ocfs2_filesys *fs)
 	       OCFS2_FEATURE_INCOMPAT_LOCAL_MOUNT;
 }
 
+int ocfs2_is_hard_readonly(ocfs2_filesys *fs)
+{
+	return fs->fs_flags & OCFS2_FLAG_HARD_RO;
+}
+
 errcode_t ocfs2_open(const char *name, int flags,
 		     unsigned int superblock, unsigned int block_size,
 		     ocfs2_filesys **ret_fs)
@@ -306,6 +311,11 @@ errcode_t ocfs2_open(const char *name, int flags,
 			block_size = fs->ost->ost_fsblksz;
 	}
 
+	/* image file is not a device */
+	if (!(flags & OCFS2_FLAG_IMAGE_FILE)) {
+		if (io_is_device_readonly(fs->fs_io))
+			fs->fs_flags |= OCFS2_FLAG_HARD_RO;
+	}
 
 	/*
 	 * If OCFS2_FLAG_NO_REV_CHECK is specified, fsck (or someone

libocfs2/unix_io.c
@@ -33,6 +33,7 @@
 #include <assert.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -40,6 +41,7 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <sys/utsname.h>
+#include <linux/fs.h>
 #endif
 #include <sys/mman.h>
 #include <inttypes.h>
@@ -616,6 +618,17 @@ static errcode_t io_validate_o_direct(io_channel *channel)
 	}
 
 	return ret;
+}
+
+int io_is_device_readonly(io_channel *channel)
+{
+	errcode_t ret;
+	int dev_ro;
+
+	ret = ioctl(channel->io_fd, BLKROGET, &dev_ro);
+	if (ret >= 0 && dev_ro)
+		return 1;
+	return 0;
 }
 
 errcode_t io_open(const char *name, int flags, io_channel **channel)

mount.ocfs2/mount.ocfs2.c
@@ -112,7 +112,7 @@ static errcode_t add_mount_options(ocfs2_filesys *fs,
 	char stackstr[strlen(OCFS2_CLUSTER_STACK_ARG) + OCFS2_STACK_LABEL_LEN + 1];
 	struct ocfs2_super_block *sb = OCFS2_RAW_SB(fs->fs_super);
 
-	if (ocfs2_mount_local(fs)) {
+	if (ocfs2_mount_local(fs) || ocfs2_is_hard_readonly(fs)) {
 		add = OCFS2_HB_NONE;
 		goto addit;
 	}
@@ -243,24 +243,6 @@ static int process_options(struct mount_options *mo)
 	return 0;
 }
 
-static int check_dev_readonly(const char *dev, int *dev_ro)
-{
-	int fd;
-	int ret;
-
-	fd = open64(dev, O_RDONLY);
-	if (fd < 0)
-		return errno;
-
-	ret = ioctl(fd, BLKROGET, dev_ro);
-	if (ret < 0)
-		return errno;
-
-	close(fd);
-
-	return 0;
-}
-
 static int run_hb_ctl(const char *hb_ctl_path,
 		      const char *device, const char *arg)
 {
@@ -324,7 +306,6 @@ int main(int argc, char **argv)
 {
 	errcode_t ret = 0;
 	struct mount_options mo;
-	int dev_ro = 0;
 	ocfs2_filesys *fs = NULL;
 	struct o2cb_cluster_desc cluster;
 	struct o2cb_region_desc desc;
@@ -364,6 +345,15 @@ int main(int argc, char **argv)
 
 	clustered = (0 == ocfs2_mount_local(fs));
 
+	if (ocfs2_is_hard_readonly(fs) && (clustered ||
+					   !(mo.flags & MS_RDONLY))) {
+		ret = OCFS2_ET_IO;
+		com_err(progname, ret,
+			"while mounting read-only device in %s mode",
+			(clustered ? "clustered" : "read-write"));
+		goto bail;
+	}
+
 	if (verbose)
 		printf("device=%s\n", mo.dev);
 
@@ -391,14 +381,6 @@ int main(int argc, char **argv)
 		desc.r_service = OCFS2_FS_NAME;
 	}
 
-	if (mo.flags & MS_RDONLY) {
-		ret = check_dev_readonly(mo.dev, &dev_ro);
-		if (ret) {
-			com_err(progname, ret, "device not accessible");
-			goto bail;
-		}
-	}
-
 	ret = add_mount_options(fs, &cluster, &mo.xtra_opts);
 	if (ret) {
 		com_err(progname, ret, "while adding mount options");
@@ -422,7 +404,7 @@ int main(int argc, char **argv)
 
 	block_signals (SIG_BLOCK);
 
-	if (!(mo.flags & MS_REMOUNT) && !dev_ro && clustered) {
+	if (clustered && !(mo.flags & MS_REMOUNT)) {
 		ret = o2cb_begin_group_join(&cluster, &desc);
 		if (ret) {
 			block_signals (SIG_UNBLOCK);