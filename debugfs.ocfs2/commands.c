markfasheh/ocfs2-tools

commit 9693851641bfcd0f2bab226e9f03d9ab05cb7edf

debugfs.ocfs2: Fix memory leak problem in process_open_args() & main()
When I was scanning the code of ocfs2-tools-1.8.4 with valgrind, two memory
leak problems were found in debugfs.ocfs2 tool. The details of the problems
are described below:

1. In process_open_args() , a char pointer 'dev' points to a heap memory
   that allocated from strdup(), but the memory was not released at the
   end of process_open_args().

2. The similar memory leak problem happens in get_logmode_sysfs(), the
   pointer current_mask() should be released after being used.

Mark:
   - Whitespace vs tab fixes
   - initialize current_mask to NULL in get_logmode_sysfs()

Signed-off-by: Jun Piao <piaojun@huawei.com>
Reviewed-by: Alex Chen <alex.chen@huawei.com>
Reviewed-by: Srinivas Eeda <srinivas.eeda@oracle.com>
Signed-off-by: Mark Fasheh <mfasheh@suse.de>

debugfs.ocfs2/commands.c
@@ -534,7 +534,6 @@ static int process_open_args(char **args,
 	int num, argc, c;
 
 	for (argc = 0; (args[argc]); ++argc);
-	dev = strdup(args[1]);
 	optind = 0;
 	while ((c = getopt(argc, args, "is:")) != EOF) {
 		switch (c) {
@@ -564,6 +563,7 @@ static int process_open_args(char **args,
 		return -1;
 	}
 
+	dev = strdup(args[1]);
 	ret = get_blocksize(dev, byte_off[s-1], &blksize, s);
 	if (ret) {
 		com_err(args[0],ret, "Can't get the blocksize from the device"
@@ -575,6 +575,7 @@ static int process_open_args(char **args,
 	*superblock = byte_off[s-1]/blksize;
 	ret = 0;
 bail:
+	g_free(dev);
 	return ret;
 }
 

debugfs.ocfs2/main.c
@@ -363,7 +363,7 @@ static int set_logmode_sysfs(const char *path, struct log_entry *entry)
 static int get_logmode_sysfs(const char *path, const char *name)
 {
 	char *logpath;
-	char *current_mask;
+	char *current_mask = NULL;
 
 	logpath = g_strdup_printf(LOG_CTL_SYSFS_FORMAT, path, name);
 	if (g_file_get_contents(logpath, &current_mask,
@@ -372,6 +372,7 @@ static int get_logmode_sysfs(const char *path, const char *name)
 	}
 	g_free(logpath);
 
+	g_free(current_mask);
 	return 0;
 }
 

commit 41057a7eef9da9d77cde46d336a7f8a19564d7f8

debugfs.ocfs2: Fix a bug in process_open_args()
In process_open_args(), 'dev' get the wrong value because getopt() will
change the value of args[1]. This problem will cause failure in
debugfs.ocfs2. ocfs2. So we should assign 'dev' before getopt().

This fix fixes the bug introduced in 9693851641bfcd0f2bab226e9f03d9ab05cb7edf

Signed-off-by: Jun Piao <piaojun@huawei.com>
Reviewed-by: Alex Chen <alex.chen@huawei.com>
Signed-off-by: Goldwyn Rodrigues <rgoldwyn@suse.com>
Reported-by: Gang He <ghe@suse.com>

debugfs.ocfs2/commands.c
@@ -534,6 +534,7 @@ static int process_open_args(char **args,
 	int num, argc, c;
 
 	for (argc = 0; (args[argc]); ++argc);
+	dev = strdup(args[1]);
 	optind = 0;
 	while ((c = getopt(argc, args, "is:")) != EOF) {
 		switch (c) {
@@ -544,26 +545,31 @@ static int process_open_args(char **args,
 				s = strtoul(optarg, &ptr, 0);
 				break;
 			default:
-				return 1;
+				ret = 1;
+				goto bail;
 				break;
 		}
 	}
 
-	if (!s)
-		return 0;
+	if (!s) {
+		ret = 0;
+		goto bail;
+	}
 
 	num = ocfs2_get_backup_super_offsets(NULL, byte_off,
 					     ARRAY_SIZE(byte_off));
-	if (!num)
-		return -1;
+	if (!num) {
+		ret = -1;
+		goto bail;
+	}
 
 	if (s < 1 || s > num) {
 		fprintf(stderr, "Backup super block is outside of valid range"
 			"(between 1 and %d)\n", num);
-		return -1;
+		ret = -1;
+		goto bail;
 	}
 
-	dev = strdup(args[1]);
 	ret = get_blocksize(dev, byte_off[s-1], &blksize, s);
 	if (ret) {
 		com_err(args[0],ret, "Can't get the blocksize from the device"