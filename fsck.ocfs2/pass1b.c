markfasheh/ocfs2-tools

commit 6b689d45289d4804647cbe51e5cb65c88131f3f7

fsck.ocfs2: dynamically allocate ocfs2_dir_entry for long filenames
Signed-off-by: Goldwyn Rodrigues <rgoldwyn@suse.com>
Signed-off-by: Srinivas Eeda <srinivas.eeda@oracle.com>

fsck.ocfs2/pass1b.c
@@ -961,12 +961,20 @@ static int walk_iterate(struct ocfs2_dir_entry *de, uint64_t blocknr,
 static void walk_cwd(struct dir_scan_context *scan)
 {
 	errcode_t ret;
-	struct ocfs2_dir_entry de;
+	struct ocfs2_dir_entry *de;
+	int len = sizeof(struct ocfs2_dir_entry);
 
-	memcpy(de.name, scan->ds_cwd, scan->ds_cwdlen);
-	de.name_len = scan->ds_cwdlen;
-	name_inode(scan, &de);
+	if (scan->ds_cwdlen > OCFS2_MAX_FILENAME_LEN)
+		len = sizeof(struct ocfs2_dir_entry) + scan->ds_cwdlen
+			- OCFS2_MAX_FILENAME_LEN;
 
+	ret = ocfs2_malloc(len, &de);
+
+	memcpy(de->name, scan->ds_cwd, scan->ds_cwdlen);
+	de->name_len = scan->ds_cwdlen;
+	name_inode(scan, de);
+	free(de);
+ 
 	ret = ocfs2_dir_iterate(scan->ds_ost->ost_fs, scan->ds_ino,
 				OCFS2_DIRENT_FLAG_EXCLUDE_DOTS, NULL,
 				walk_iterate, scan);

commit 11287a0f9cc81593c2f5c6431d038b379bb02f38

fsck.ocfs2: warn and return if ocfs2_malloc fails in walk_cwd
In walk_cwd, we should check return code in case of ocfs2_malloc fails.
Otherwise, segment fault will happen.

Fixes: 6b689d45289d ("fsck.ocfs2: dynamically allocate ocfs2_dir_entry
for long filenames")
Signed-off-by: Jun Piao <piaojun@huawei.com>
Reviewed-by: Joseph Qi <joseph.qi@huawei.com>

fsck.ocfs2/pass1b.c
@@ -969,6 +969,10 @@ static void walk_cwd(struct dir_scan_context *scan)
 			- OCFS2_MAX_FILENAME_LEN;
 
 	ret = ocfs2_malloc(len, &de);
+	if (ret) {
+		pass1c_warn(ret);
+		return;
+	}
 
 	memcpy(de->name, scan->ds_cwd, scan->ds_cwdlen);
 	de->name_len = scan->ds_cwdlen;