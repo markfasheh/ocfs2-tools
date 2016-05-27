markfasheh/ocfs2-tools

commit 15f01d8d8c1c1ad1c8462d9dabb2ca1e4d11c86e

tunefs.ocfs2: Warn if not using complete device
Sample test case (for a device greater than 16TB):
1. Format a device with blocksize 4k and block count < 16TB
2. resize device (-S)
Results: Device is formatted to 16TB even if the device is bigger.

The patch fails the tunefs.ocfs2 attempt and ask user to explicitly state
block-count to not use the complete device.

Signed-off-by: Goldwyn Rodrigues <rgoldwyn@suse.de>
Signed-off-by: Michal Srb <msrb@suse.com>
Signed-off-by: Sunil Mushran <sunil.mushran@oracle.com>

tunefs.ocfs2/op_resize_volume.c
@@ -535,10 +535,24 @@ static errcode_t check_new_size(ocfs2_filesys *fs, uint64_t new_size,
 	}
 
 	device_clusters = device_blocks >> b_to_c_bits;
-	if (device_clusters > UINT32_MAX)
-		device_clusters = UINT32_MAX;
-	if (!try_clusters)
+
+	if (!try_clusters) {
+		if (device_clusters > UINT32_MAX) {
+			verbosef(VL_APP,
+				 "Clusters (%"PRIu64" is greater than "
+				 "maximum possible clusters %"PRIu32"\n",
+				 device_clusters, UINT32_MAX);
+			try_blocks = UINT32_MAX << b_to_c_bits;
+			errorf("Clusters %"PRIu64" is greater than max allowed"
+			       " %"PRIu32".\nIf you want to resize to %"PRIu64
+			       " blocks please specify %"PRIu64" as "
+			       "blocks-count.\n", device_clusters,
+				UINT32_MAX, try_blocks, try_blocks);
+			return TUNEFS_ET_INVALID_NUMBER;
+		}
 		try_clusters = device_clusters;
+	}
+
 	try_blocks = try_clusters << b_to_c_bits;
 
 	/* Now we're guaranteed that try_clusters is within range */

commit 26411649a527420156643c97c5f860d182e3a8c5

tunefs.ocfs2: fix a potential overflowing problem in check_new_size
Potentially overflowing expression "0xffffffffU << b_to_c_bits" with
type "unsigned int" (32 bits, unsigned) is evaluated using 32-bit
arithmetic before being used in a context which expects an expression
of type "uint64_t" (64 bits, unsigned). To avoid overflow, cast the left
operand to "uint64_t" before performing the left shift.

Fixes: 15f01d8d8c1c ("tunefs.ocfs2: Warn if not using complete device")
Signed-off-by: Jun Piao <piaojun@huawei.com>
Reviewed-by: Gang He <ghe@suse.com>
Signed-off-by: Joseph Qi <joseph.qi@huawei.com>

tunefs.ocfs2/op_resize_volume.c
@@ -543,7 +543,7 @@ static errcode_t check_new_size(ocfs2_filesys *fs, uint64_t new_size,
 				 "Clusters (%"PRIu64" is greater than "
 				 "maximum possible clusters %"PRIu32"\n",
 				 device_clusters, UINT32_MAX);
-			try_blocks = UINT32_MAX << b_to_c_bits;
+			try_blocks = (uint64_t)UINT32_MAX << b_to_c_bits;
 			errorf("Clusters %"PRIu64" is greater than max allowed"
 			       " %"PRIu32".\nIf you want to resize to %"PRIu64
 			       " blocks please specify %"PRIu64" as "