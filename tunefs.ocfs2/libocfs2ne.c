markfasheh/ocfs2-tools

commit 79ec217ccd03dbe68bce8338e4a39489aa1eed2a

tunefs.ocfs2: fix memory leak problems in tunefs_journal_check() and parse_options()
Two memory leak problems were found in tunefs.ocfs2 described as bellow:
1. Free 'ci' in each cycle of the loop in tunefs_journal_check();
2. Free 'long_options' and 'optstring' at the end of parse_options().

Signed-off-by: Jun Piao <piaojun@huawei.com>
Reviewed-by: Joseph Qi <joseph.qi@huawei.com>

tunefs.ocfs2/libocfs2ne.c
@@ -1794,6 +1794,7 @@ static errcode_t tunefs_journal_check(ocfs2_filesys *fs)
 			jsb->s_feature_ro_compat;
 		state->ts_journal_features.opt_incompat |=
 			jsb->s_feature_incompat;
+		ocfs2_free_cached_inode(fs, ci);
 	}
 
 	/*

tunefs.ocfs2/ocfs2ne.c
@@ -1002,6 +1002,8 @@ static errcode_t parse_options(int argc, char *argv[], char **device)
 	}
 
 out:
+	ocfs2_free(&optstring);
+	ocfs2_free(&long_options);
 	return err;
 }
 

commit 8c7a3b505f1f2fac8d926d975fdd0f4efcbaef1a

tunefs.ocfs2: fix double free bug
Although it will set cinode to NULL in ocfs2_free, it won't affect the
outer ci in tunefs_journal_check, so we have to set ci to NULL
explicitly here.

Fixes: 79ec217ccd03("tunefs.ocfs2: fix memory leak problems in tunefs_journal_check() and parse_options()")
Signed-off-by: Junxiao Bi <junxiao.bi@oracle.com>
Reviewed-by: Joseph Qi <joseph.qi@huawei.com>
Reviewed-by: Jun Piao <piaojun@huawei.com>

tunefs.ocfs2/libocfs2ne.c
@@ -1795,6 +1795,7 @@ static errcode_t tunefs_journal_check(ocfs2_filesys *fs)
 		state->ts_journal_features.opt_incompat |=
 			jsb->s_feature_incompat;
 		ocfs2_free_cached_inode(fs, ci);
+		ci = NULL;
 	}
 
 	/*