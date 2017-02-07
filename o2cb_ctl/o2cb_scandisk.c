ebe865124d3ac26406afe374cc620c207caf7ec7
Frank Brendel <frank.brendel@eurolog.com>
2017-02-07 08:58:35 +0100

    support Ceph block devices as global heartbeat

--- o2cb_ctl/o2cb_scandisk.c
+++ o2cb_ctl/o2cb_scandisk.c
@@ -136,6 +136,8 @@ static void add_to_list(struct list_head *device_list, struct devnode *node)
 				add = !strncmp(path->path, "/dev/xvd", 8);
 			if (!add)
 				add = !strncmp(path->path, "/dev/vd", 7);
+			if (!add)
+				add = !strncmp(path->path, "/dev/rbd", 8);
 		}
 		if (add) {
 			hb = malloc(sizeof(struct hb_devices));
