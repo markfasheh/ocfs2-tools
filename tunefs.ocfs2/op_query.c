markfasheh/ocfs2-tools

commit 3e692a9a0ee2c2ff4db69b6b431f34c8bfd107d6

vendor: rhel5: fix build failure
register_printf_specifier() doesn't exist in old OS like rhel5,
revert back to use register_printf_function().

Fixes: 5644bfc73930("tunefs.ocfs2: fix build warnings")
Signed-off-by: Junxiao Bi <junxiao.bi@oracle.com>
Reviewed-by: Joseph Qi <jiangqi903@gmail.com>

tunefs.ocfs2/op_query.c
@@ -324,19 +324,19 @@ static int query_run(struct tunefs_operation *op, ocfs2_filesys *fs,
 		return 1;
 	}
 
-	register_printf_specifier('B', handle_blocksize, handle_arginfo);
-	register_printf_specifier('T', handle_clustersize, handle_arginfo);
-	register_printf_specifier('N', handle_numslots, handle_arginfo);
-	register_printf_specifier('R', handle_rootdir, handle_arginfo);
-	register_printf_specifier('Y', handle_sysdir, handle_arginfo);
-	register_printf_specifier('P', handle_clustergroup, handle_arginfo);
+	register_printf_function('B', handle_blocksize, handle_arginfo);
+	register_printf_function('T', handle_clustersize, handle_arginfo);
+	register_printf_function('N', handle_numslots, handle_arginfo);
+	register_printf_function('R', handle_rootdir, handle_arginfo);
+	register_printf_function('Y', handle_sysdir, handle_arginfo);
+	register_printf_function('P', handle_clustergroup, handle_arginfo);
 
-	register_printf_specifier('V', handle_label, handle_arginfo);
-	register_printf_specifier('U', handle_uuid, handle_arginfo);
+	register_printf_function('V', handle_label, handle_arginfo);
+	register_printf_function('U', handle_uuid, handle_arginfo);
 
-	register_printf_specifier('M', handle_compat, handle_arginfo);
-	register_printf_specifier('H', handle_incompat, handle_arginfo);
-	register_printf_specifier('O', handle_ro_compat, handle_arginfo);
+	register_printf_function('M', handle_compat, handle_arginfo);
+	register_printf_function('H', handle_incompat, handle_arginfo);
+	register_printf_function('O', handle_ro_compat, handle_arginfo);
 
 	query_fs = fs;
 	fprintf(stdout, fmt);

commit a5f3d6c6d3519c5e2d5b03debea2b4b452bcb56a

tunefs.ocfs2: fix compile warning
Using correct prototype of handle_arginfo to clear the following
compile warning:

op_query.c:328:2: warning: passing argument 3 of ‘register_printf_function’ from incompatible pointer type [enabled by default]
In file included from op_query.c:21:0:
/usr/include/printf.h:106:12: note: expected ‘int (*)(const struct printf_info *, size_t,  int *)’ but argument is of type ‘int (*)(const struct printf_info *, size_t,  int *, int *)’
 extern int register_printf_function (int __spec, printf_function __func,
            ^

Fixes: 3e692a9a0ee2 ("vendor: rhel5: fix build failure")
Signed-off-by: Joseph Qi <jiangqi903@gmail.com>
Cc: Junxiao Bi <junxiao.bi@oracle.com>

tunefs.ocfs2/op_query.c
@@ -222,8 +222,7 @@ static int handle_ro_compat(FILE *stream, const struct printf_info *info,
 	return len;
 }
 
-static int handle_arginfo(const struct printf_info *info, size_t n, int *types,
-		int *size)
+static int handle_arginfo(const struct printf_info *info, size_t n, int *types)
 {
 	return 0;
 }